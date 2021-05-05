f2f447948c73dae0497fa430536341405d596fdd
imadcollin <imad.collin@gmail.com>
2021-05-05 23:46:10 +0200

    Add lock file

--- Ch1.js
+++ Ch1.js
@@ -22,3 +22,33 @@ const joinEl = unsplat(function (array) {
 });
 
 console.log(joinEl(1, 2));
+
+/****** Functions as Units of Abstraction  ****** */
+
+function parseAge(age) {
+  if (!_.isString(age)) {
+    console.log("String is expected...!");
+    return 0;
+  }
+  let a = parseInt(age, 10);
+  if (_.isNaN(a)) {
+    console.log("can not be parced", age).join(" ");
+    a = 0;
+  }
+  return a;
+}
+console.log(parseAge(40));
+console.log(parseAge("40"));
+
+/*Warnings */
+
+function warn(thing) {
+  console.log(["WARNING:", thing].join(" "));
+}
+
+function note(thing) {
+  console.log(["NOTE:", thing].join(" "));
+}
+warn();
+
+note();
